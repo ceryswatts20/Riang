b715e861f666590007c13a55887bb7a98877a1e4
Cerys Watts <ceryswatts942@gmail.com>
2023-01-13 11:13:09 +0000

    Initial Commit

---
+++ Riang/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Riang
+//
+//  Created by Cerys Watts on 13/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Riang/RiangApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiangApp.swift
+//  Riang
+//
+//  Created by Cerys Watts on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct RiangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
